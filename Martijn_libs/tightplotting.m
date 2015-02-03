f24c30ec2d6c8d72160800eedfa1d75eefa5ab7e
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2015-02-03 18:11:19 +0100

    (libs) tighplotting doesn't work yet.

--- Martijn_libs/tightplotting.m
+++ Martijn_libs/tightplotting.m
@@ -1,4 +1,19 @@
 
+
+
+% Both codes below don't work??
+%{
+pos = get(gcf, 'pos');
+myWidth = pos(3)-pos(1);
+myHeight = pos(4)-pos(2);
+set(gca,'units','pixels');
+set(gcf, 'PaperPosition', [0 0 myWidth myHeight]); %Position plot at left hand corner with width 5 and height 5.
+set(gcf, 'PaperSize', [myWidth myHeight]); %Set the paper to have width 5 and height 5.
+%}
+
+%{
+
+
 % MW 2015/02
 % 
 % Source from:
@@ -6,12 +21,12 @@
 
 % Make your figure boundaries tight:
 
-ti = get(gca,'TightInset')
+ti = get(gca,'TightInset');
 set(gca,'Position',[ti(1) ti(2) 1-ti(3)-ti(1) 1-ti(4)-ti(2)]);
 
 % Now you have a tight figure on the screen but if you directly do saveas (or print), MATLAB will still add the annoying white space. To get rid of them, we need to adjust the ``paper size":
 
-set(gca,'units','centimeters')
+set(gca,'units','centimeters');
 pos = get(gca,'Position');
 ti = get(gca,'TightInset');
 
@@ -20,3 +35,4 @@ set(gcf, 'PaperSize', [pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
 set(gcf, 'PaperPositionMode', 'manual');
 set(gcf, 'PaperPosition',[0 0 pos(3)+ti(1)+ti(3) pos(4)+ti(2)+ti(4)]);
 
+%}
