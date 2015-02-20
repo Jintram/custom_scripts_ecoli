25f2f259a6be30cf3dfa2a7493b5ec8269a904c3
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2015-02-20 16:47:15 +0100

    (movies) alternative movie; also lineage distance plotting.

--- Martijn_custom/movies_lineage_distance.m
+++ Martijn_custom/movies_lineage_distance.m
@@ -1,9 +1,38 @@
 
 p = DJK_initschnitz('pos8crop','2014-05-01','e.coli.AMOLF','rootDir','F:\A_Tans1_step4a_partially_analyzed_analysis\', 'cropLeftTop', [1,1], 'cropRightBottom', [1392,1040],'fluor1','none','fluor2','none','fluor3','none');
-myRange=[133:150]
+myRange=[39:130]
 
 % For plotting lengths
 [p,schnitzcells] = DJK_compileSchnitzImproved_3colors(p,'quickMode',1);
+
+% Now obtain the schnitzes that are living at the end of the movie
+lastFrame = myRange(end);
+[youngSchnitzes,cellTimePoints,cellLengths,timemin,timemax,lengthmin,lengthmax] =  ...
+        givemeschnitzinfoforframe(p, schnitzcells, lastFrame);
+
+% Relating all schnitzes to their final offspring can best be done going 
+% backwards into the lineage.
+YoungestOffspringForSchnitz = cell(size(schnitzcells));
+for youngGun = youngSchnitzes 
+    % go back into ancestry until we have already seen this ancestor in
+    % another search
+    currentSchnitz = youngGun;
+    done = 0;
+    while ~done       
+        if (isempty(currentSchnitz) ||  (currentSchnitz == 0))
+            done = 1; % we're at the oldest schnitz in this lineage; done
+        else
+            % record that current schnitz has "youngGun" as final offspring
+            YoungestOffspringForSchnitz{currentSchnitz}(end+1) = youngGun;
+            % update current schnitz to its parent
+            currentSchnitz = schnitzcells(currentSchnitz).P;
+        end                
+    end
+end
+
+% Now we make a colormap for each schnitz, comprising of a mix color of its
+% parents.
+% First pick some colours for the young guns:
 
 % figure
 h=figure(1), set(h, 'Position', [0,0,800,500]);
@@ -32,7 +61,7 @@ for fr = myRange
   % Now also plot length
   % ===
   
-  [theSchnitzes,cellTimePoints,cellLengths,timemin,timemax,lengthmin,lengthmax] =  givemeschnitzinfoforframe(p, schnitzcells, fr);
+  [theSchnitzes,cellTimePoints,cellLengths,timemin,timemax,lengthmin,lengthmax] =  [theSchnitzes,cellTimePoints,cellLengths,timemin,timemax,lengthmin,lengthmax] =  givemeschnitzinfoforframe(p, schnitzcells, fr);
   
   subplot(1,2,2), hold on 
   for j = 1:numel(theSchnitzes)
