848b733bbe6ef3668368eedb8164901b28ac827a
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2015-02-28 20:06:57 +0100

    (PaxDBabundances) Cosmetic fix (comments).

--- Martijn_custom/regulationproject_abundances.m
+++ Martijn_custom/regulationproject_abundances.m
@@ -1,10 +1,15 @@
 function PPMs = regulationproject_abundances(strPax)
-
+% Return ppm value for enzyme measured by different databases.
+% Input is id from PaxDB database. (Look for gene name in database%
+% to obtain that ID.
+% 
 % Execute e.g. by 
 % regulationproject_abundances('b4025')
 
+% The available databases (excl. Integrated one)
 databases = {'Lu', 'Lewis', 'Mancuso', 'Kim', 'Wright', 'Taniguchi'};
 
+% Empty output array
 PPMs = NaN(1,numel(databases));
 
 % Load dataset if not detected
@@ -17,15 +22,19 @@ end
 % For all separate databses
 outputstr = '';
 for i = 1:numel(databases)   
+    % Look for ID
     eval(['hitidx = findInDB(strPax, ' databases{i} ');'])    
     if isempty(hitidx) 
-        PPMs(i) = NaN;
+        % If not found, flag by NaN
+        PPMs(i) = NaN; 
     else
-        eval(['PPMs(' num2str(i) ') = cell2mat(' databases{i} '(hitidx,3));']);
+        % otherwise add number
+        eval(['PPMs(' num2str(i) ') = cell2mat(' databases{i} '(hitidx,3));']); 
     end
     outputstr = [outputstr, num2str(PPMs(i)), '\t '];
 end
 
+% Print to user
 sprintf(outputstr)
 outputstr = strrep(outputstr, '.', ',');
 sprintf(outputstr)
@@ -43,7 +52,8 @@ outputstr2 = num2str(IntegratedPPM);
 outputstr2 = strrep(outputstr2, '.', ',');
 sprintf(['Integrated= ', outputstr2])
 
-hitidx
+% index in matlab table
+hitidx 
 
 end
 
@@ -51,13 +61,18 @@ end
 
 
 function theIndex = findInDB(strPax,database) 
+% Return index of line that contains gene with id strPax in database
 
-strcmp(strPax,database);
+%strcmp(strPax,database);
+
+% Look for strPax id in database
 indices = strfind(database(:,2),strPax);
+% Fill empty cells with zeros and convert to matrix
 ix=cellfun(@isempty,indices);
 indices(ix)={0}; 
 indices = cell2mat(indices);
 
+% Spit out index
 theIndex = find(indices);
 
 end
