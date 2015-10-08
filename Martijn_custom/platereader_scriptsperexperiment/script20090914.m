c4b60e5fcb87392ab1d564c6e9e5d182407cf57d
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2015-10-08 11:39:00 +0200

    (minor)

--- Martijn_custom/platereader_scriptsperexperiment/script20090914.m
+++ Martijn_custom/platereader_scriptsperexperiment/script20090914.m
@@ -1,5 +1,6 @@
 
 
+
 %% Parameter settings
 USERSETTINGS.myRootDir='U:\ZZ_EXPERIMENTAL_DATA\C_Platereader\';
 USERSETTINGS.myDateDir='2015_09_01_exp1\';
@@ -8,97 +9,61 @@ USERSETTINGS.customSuffix = '_GFP';
 USERSETTINGS.ODorFluor = 2;
 
 
-myConcentrationValues = [  ...
-        0.001 ,...
-        0.0003 ,...
-        0.00009 ,...
-        0.000027 ,...
-        0.0000081 ,...
-        0
-        ];
+USERSETTINGS.myConcentrationValues = ... % 0.0031 M start, % 3.1622X dilution series
+        [(94/2994*.1) ./ (3.1622 .^ [0 1 2 3 4 5 6]) 0]
 
-myXlim = [1e-6,max(myConcentrationValues)*10];
+USERSETTINGS.myXlim = [1e-6,max(USERSETTINGS.myConcentrationValues)*10];
 
 % Which points to take into account for fit, in relation to
 % myConcentrationValues defined above
-STARTAT = 1;
-ENDIGNORE = 0;
+USERSETTINGS.STARTAT = 1;
+USERSETTINGS.ENDIGNORE = 0;
 
 
 %% Call script for first batch
-DONTSAVE=1;
-USERSETTINGS.wellNamesToPlot = ...
-    {'str38A', 'str38B', 'str38C', 'str38D', 'str38E', 'str38F'};
-ExtractFitPlateReaderData_General_Part2_Fluor
-
-%%
-figure(88), clf, hold on
+myhandle=figure(88), clf, hold on;
 legendLinesHandles = [];
 
-% y=zero line
-plot(myXlim, [0, 0], 'k-','LineWidth',2)
-
-for i = 1:numel(output)
-    l=plot(   ones(1,numel(output(i).manualMuValues)) * myConcentrationValues(i), ...
-                output(i).manualMuValues,'o')
-    set(l, 'MarkerSize', 10,'LineWidth',3,'Color','b');   
-end
-legendLinesHandles(end+1) = l;
-% value at zero
-plot(myXlim,[1,1]*mean(output(end).manualMuValues),'--b','LineWidth',3);
-
-% Some settings for the plot
-set(gca,'xscale','log');
-
-MW_makeplotlookbetter(20);
-
-ylim([-.1,1]);
-xlim(myXlim);
-
-xlabel('cAMP concentration [M]');
-ylabel('fitted growth rate [dbl/hr]');
-
-%% now retrieve the cAMP extracell. data
-
+% s70 data WT strain
 DONTSAVE=1;
 USERSETTINGS.wellNamesToPlot = ...
-    {'str39A', 'str39B', 'str39C', 'str39D', 'str39E', 'str39F'};
+{'str841A','str841B','str841C','str841D','str841E','str841F','str841G','str841H'};
 ExtractFitPlateReaderData_General_Part2_Fluor
-
-%% and plot it
-figure(88), hold on;
-for i = 1:numel(output)
-    l=plot(   ones(1,numel(output(i).manualMuValues)) * myConcentrationValues(i), ...
-                output(i).manualMuValues,'s')
-    set(l, 'MarkerSize', 10,'LineWidth',3,'Color', 'r');
+[allLinehandles, legendLinesHandles] = platereaderextraplot(USERSETTINGS,output,myhandle,legendLinesHandles,[.6 .6 1])
+for l = allLinehandles
+    set(l, 'Marker', 's', 'MarkerSize', 10,'LineWidth',2);
 end
-legendLinesHandles(end+1) = l;
-% value at zero
-plot(myXlim,[1,1]*mean(output(end).manualMuValues),'--r','LineWidth',3);
 
-% Make a fit
-%{
-meanValues = [];
-disp('Fitting, ignoring last point.')
-for i = STARTAT:(numel(output)-ENDIGNORE) % last point, [cAMP] zero
-    meanValues(end+1) = mean(output(i).manualMuValues);    
+% s70 data delta strain
+DONTSAVE=1;
+USERSETTINGS.wellNamesToPlot = ...
+{'str894A','str894B','str894C','str894D','str894E','str894F','str894G','str894H'};
+ExtractFitPlateReaderData_General_Part2_Fluor
+[allLinehandles, legendLinesHandles] = platereaderextraplot(USERSETTINGS,output,myhandle,legendLinesHandles,[1 .6 .6])
+for l = allLinehandles
+    set(l, 'Marker', 's', 'MarkerSize', 10,'LineWidth',2);
 end
-toFitX = myConcentrationValues(STARTAT:end-ENDIGNORE)/log(10);
-p=polyfit(toFitX,meanValues,2)
 
-fittedXs=linspace(myXlim(1),myXlim(2),100);
-logFittedXs=log(fittedXs)/log(10);
-fittedYs=p(1)*logFittedXs.^2 + p(2).*logFittedXs + p(3);
-%figure
-plot(fittedXs,fittedYs,'-r','LineWidth',2);
-set(gca,'xscale','log');
+% CRP data WT strain
+DONTSAVE=1;
+USERSETTINGS.wellNamesToPlot = ...
+{'str842A','str842B','str842C','str842D','str842E','str842F','str842G','str842H'};
+ExtractFitPlateReaderData_General_Part2_Fluor
+[allLinehandles, legendLinesHandles] = platereaderextraplot(USERSETTINGS,output,myhandle,legendLinesHandles,'b')
+% CRP data delta strain 
+DONTSAVE=1;
+USERSETTINGS.wellNamesToPlot = ...
+{'str893A','str893B','str893C','str893D','str893E','str893F','str893G','str893H'};
+ExtractFitPlateReaderData_General_Part2_Fluor
+[allLinehandles, legendLinesHandles] = platereaderextraplot(USERSETTINGS,output,myhandle,legendLinesHandles,'r')
 
-% Gaussian fit
-f = fit(toFitX',meanValues','gauss2')
-%}
-%% 
+%***
 
-legend(legendLinesHandles, {'838..','839..'});
+legend(legendLinesHandles,{'s70 WT','s70 delta','CRP WT','CRP delta'},'Location','northeastoutside')
+
+%% And plot
+
+
 
 
 
