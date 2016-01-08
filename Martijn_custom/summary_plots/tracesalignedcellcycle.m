650e63c21af492840f367e9c52f33dc0212a0ff4
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2016-01-08 20:18:01 +0100

    (update)

--- Martijn_custom/summary_plots/tracesalignedcellcycle.m
+++ Martijn_custom/summary_plots/tracesalignedcellcycle.m
@@ -4,15 +4,23 @@
 % Here I just make some plots of cellular traces with t0 = division.
 
 %% Load some schnitzcells dataset
-if ~exist(MYDATASET)
+if ~exist('MYDATASET','var')
     MYDATASET='F:\A_Tans1_step1_incoming_not_backed_up\2015-10-20\pos4crop\data\pos4crop-Schnitz.mat';
+    disp('Loaded default dataset');
 end
 load(MYDATASET);
 
+if ~exist('schnitzcells','var')    
+    schnitzcells=s_rm;
+end
+
+if ~exist('speedfield')
+    speedfield = 'mu5_fitNew';
+end
 
 %% Get all cell traces into one cell (this is actually not necessary as we
 % could have also used "schnitzcells.mu5_fitNew" all the time).
-traces = {schnitzcells.mu5_fitNew};
+traces = {schnitzcells.(speedfield)};
 maxTraceLength = max(cellfun('length',traces)); % max trace length in frames
 
 % make figure
@@ -44,22 +52,23 @@ violin(extendedtraces);
 stdovermeanPerFrame = [];
 stdovermeanPerFrameForExtended = [];
 for i=1:maxTraceLength
-    stdovermeanPerFrame(i) = std(musPerFrame{i})/mean(musPerFrame{i})
-    stdovermeanPerFrameForExtended(i) = std(extendedtraces(:,i))/mean(extendedtraces(:,i))
+    stdovermeanPerFrame(i) = std(musPerFrame{i})/mean(musPerFrame{i});
+    indexNoNaN=~isnan(extendedtraces(1,:));
+    stdovermeanPerFrameForExtended(i) = std(extendedtraces(indexNoNaN,i))/mean(extendedtraces(indexNoNaN,i));
 end
 
 % plot of summary parameter
 figure(3); clf; hold on;
 %h=bar(stdovermeanPerFrame)
-h=bar(stdovermeanPerFrameForExtended)
+h=bar(stdovermeanPerFrameForExtended);
 set(h, 'FaceColor', [.7 .7 .7]);
 
-ylabel('Coefficient of variation (CV) (std/mean)')
+ylabel('Coefficient of variation (CV) (std/mean)');
 xlabel('Time in cell cycle [frames]');
 MW_makeplotlookbetter(15);
 
 xlim([0, maxTraceLength+1]);
-ylim([0,max(stdovermeanPerFrameForExtended)*1.1])
+ylim([0,max(stdovermeanPerFrameForExtended)*1.1]);
 
 
 
