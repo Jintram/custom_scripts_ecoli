3ba77457d15623eaa1cebee4ad5d20315e93924e
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2016-04-29 19:13:39 +0200

    (filamentationRecovery) more extensive plotting

--- Martijn_custom/filamentationrecovery/script20160429_filamentRecoveryDivisionRatioss.m
+++ Martijn_custom/filamentationrecovery/script20160429_filamentRecoveryDivisionRatioss.m
@@ -3,6 +3,14 @@
 % Plotting division ratios
 %
 
+HISTNRBINS=20;
+
+FIGURENUMBERS=[1 1 1];
+
+LENGTHFIELD = 'areaPixels';
+%LENGTHFIELD = 'length_fitNew';
+%LENGTHFIELD = 'length_skeleton';
+
 %% The sulA datasets
 
 datasetsPaths = ...
@@ -12,20 +20,25 @@ datasetsPaths = ...
     'G:\EXPERIMENTAL_DATA_2016\a_incoming\2016-04-08_sulA_recovery_200uM_IPTG\pos7crop\data\pos7crop-Schnitz.mat',...
     }
 
+%% 
+for i = FIGURENUMBERS
+    figure(i); clf; hold on;
+end
 
+%%
 
-for datasetIdx = 1:3 
+for datasetIdx = 1:numel(datasetsPaths)
     
     load(datasetsPaths{datasetIdx});
 
     %% 
-    FIGURE=datasetIdx;
-    LENGTHFIELD = 'areaPixels';
-    %LENGTHFIELD = 'length_fitNew';
-    %LENGTHFIELD = 'length_skeleton'
+    figureIndex=FIGURENUMBERS(datasetIdx);
 
     %%
 
+    
+    % Finding parent with each daughter
+    %{
     myLengthNewborns = []; myLengthParents = [];
     for i = 1:numel(schnitzcells)
 
@@ -43,7 +56,41 @@ for datasetIdx = 1:3
         end
 
     end
+    %}
+    
+    % Finding 2 daughters with each parent
+    myLengthNewborns{datasetIdx} = []; myLengthParents{datasetIdx} = []; myLengthSumNewborns{datasetIdx}=[];
+    for i = 1:numel(schnitzcells)
 
+        LengthParent = schnitzcells(i).(LENGTHFIELD)(end);
+        
+        daughterSchnitz1 = schnitzcells(i).D;
+        daughterSchnitz2 = schnitzcells(i).E;
+
+        if ~any([daughterSchnitz1,daughterSchnitz2]==0)
+
+            lengthDaughterSchnitz1 = schnitzcells(daughterSchnitz1).(LENGTHFIELD)(1);
+            lengthDaughterSchnitz2 = schnitzcells(daughterSchnitz2).(LENGTHFIELD)(1);            
+            
+            % daughter 1
+            myLengthNewborns{datasetIdx}(end+1) =     lengthDaughterSchnitz1;
+            myLengthParents{datasetIdx}(end+1) =      LengthParent;
+            myLengthSumNewborns{datasetIdx}(end+1) =  lengthDaughterSchnitz1+lengthDaughterSchnitz2;
+            % daughter 2
+            myLengthNewborns{datasetIdx}(end+1) =     lengthDaughterSchnitz2;
+            myLengthParents{datasetIdx}(end+1) =      LengthParent;
+            myLengthSumNewborns{datasetIdx}(end+1) =  lengthDaughterSchnitz1+lengthDaughterSchnitz2;
+
+        end
+
+    end
+
+end
+
+%% Now plot this
+Ratios={};
+for datasetIdx = 1:numel(datasetsPaths)
+    
     %%
     % user given parameters
     LONGESTNORMALDIVSIZEPARENT = 2000;
@@ -59,7 +106,7 @@ for datasetIdx = 1:3
     end
 
     % create figure
-    figure(FIGURE); clf; hold on;
+    figure(figureIndex); 
 
     % plot helping lines at 1/2n
     % for i=1:5
@@ -73,17 +120,15 @@ for datasetIdx = 1:3
         end
     end
 
-    % plot ratios
-    Ratios = myLengthNewborns./myLengthParents;
-    plot(myLengthParents,Ratios,'xb','LineWidth',2);
-    plot(myLengthParents,1-Ratios,'xr','LineWidth',2);
-
     % target length line
     x = (LONGESTNORMALDIVSIZEPARENT):rightx;
     y = (LONGESTNORMALDIVSIZEPARENT/2)./x;
-    plot(x,y,'-','Color',[.5 .5 .5],'LineWidth',2);
-
-
+    plot(x,y,'-','Color',[.5 .5 .5],'LineWidth',2);    
+    
+    % plot ratios
+    Ratios{datasetIdx} = myLengthNewborns{datasetIdx}./myLengthSumNewborns{datasetIdx};
+    plot(myLengthSumNewborns{datasetIdx},Ratios{datasetIdx},'xb','LineWidth',2);
+    plot(myLengthSumNewborns{datasetIdx},1-Ratios{datasetIdx},'ob','LineWidth',2);
 
     % cosmetics
     ylim([0,1]);
@@ -96,9 +141,75 @@ for datasetIdx = 1:3
 
 end
 
+%% Now calculate overall histogram
+h=figure(FIGURENUMBERS(end)+1); clf; hold on;
+
+% helping lines
+N=5; highestcount=max(count);
+for i=1:N
+    for j = 1:(i*2-1)
+        plot([(j)/(2*i) (j)/(2*i)],[0, highestcount],'-','Color',[.5 .5 .5],'LineWidth',N-i+1)
+    end
+end
+
+% hist
+[count,x] = hist([Ratios{:}],HISTNRBINS);
+
+if exist('histSkel','var') && exist('histArea','var')
+    % plot histograms from multiple fields
+    l1=plot(histArea(2,:),histArea(1,:),'-','LineWidth',3);    
+    l2=plot(histSkel(2,:),histSkel(1,:),'-','LineWidth',3);
+    legend([l1,l2],{'Skeleton','Area'});
+else
+    % plotting of one histogram
+    plot(x,count,'-','LineWidth',3);
+end
+
+% additional ticks
+plot([0:.1:1],zeros(1,11),'+','Color','k');%,'MarkerFaceColor','k');
+
+% cosmetics
+%ax=gca; ax.XTick = [0:.1:1];
+ylabel('Count');
+xlabel('L_d/L_p');
+
+set(gca,'XTick',[0:.1:1])
+
+ylim([0 highestcount]);
+
+MW_makeplotlookbetter(15);
+
+% Save the histogram
+if strcmp(LENGTHFIELD, 'areaPixels')
+    histArea=[count;x]
+elseif strcmp(LENGTHFIELD, 'length_skeleton')
+    histSkel=[count;x]
+end
+
+%% Sanity check
+% load(datasetsPaths{2})
+figure(FIGURENUMBERS(end)+2); clf; hold on
+
+% x=y line
+plot([1,10^6],[1,10^6],'-k')
+
+% data
+for i=1:numel(datasetsPaths)
+    plot(myLengthSumNewborns{datasetIdx}, myLengthParents{datasetIdx},'x')
+end
+
+% cosmetics
+axis equal;
+xlim([0 max([myLengthSumNewborns{:}])]);
+ylim([0 max([myLengthParents{:}])]);
 
 
 
+%%
+figure(FIGURENUMBERS(end)+3); clf; hold on;
+
+plot(histArea(2,:),histArea(1,:),'-r');
+plot(histSkel(2,:),histSkel(1,:),'-b');
 
 
 
