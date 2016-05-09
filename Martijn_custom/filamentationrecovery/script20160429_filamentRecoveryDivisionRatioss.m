30a2da125d6569f2f7fd916e3d1e20ed9a59737b
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2016-05-09 19:57:37 +0200

    (filemantationRecovery) Ratio scripts. Added windowed histograms and interdivision vs. length.

--- Martijn_custom/filamentationrecovery/script20160429_filamentRecoveryDivisionRatioss.m
+++ Martijn_custom/filamentationrecovery/script20160429_filamentRecoveryDivisionRatioss.m
@@ -1,8 +1,8 @@
 
 %% 
 % Plotting division ratios
-%WHATDATA = 'sulA';
-WHATDATA = 'temperature';
+WHATDATA = 'sulA';
+%WHATDATA = 'temperature';
 
 HISTNRBINS=20;
 
@@ -14,6 +14,10 @@ PLOTCOLORS = [255, 0, 0; 204, 0, 204 ; 255, 102, 0]./255; % shades of blue
 %LENGTHFIELD = 'areaPixels';
 %LENGTHFIELD = 'length_fitNew';
 LENGTHFIELD = 'length_skeleton';
+
+PLOTSAVEDIR = 'U:\PROJECTS\B_filamentationRecovery\Contributions_Martijn\figures_fig_files\';
+makenoteintoreadme(PLOTSAVEDIR,'script20160429_filamentRecoveryDivisionRatioss');
+
 
 %% The sulA datasets
 
@@ -27,16 +31,33 @@ if strcmp(WHATDATA, 'sulA')
 elseif strcmp(WHATDATA, 'temperature')
     datasetsPaths = ...
         { ...
-        ...'G:\EXPERIMENTAL_DATA_2016\a_incoming\2016-03-23\pos4crop\data\pos4crop-Schnitz.mat',...
+        'G:\EXPERIMENTAL_DATA_2016\a_incoming\2016-03-23\pos4crop\data\pos4crop-Schnitz.mat',...
         ['G:\EXPERIMENTAL_DATA_2016\a_incoming\2016-04-07\pos' num2str(2) 'crop\data\pos' num2str(2) 'crop-Schnitz.mat'],...
         }
+else
+    error('No data loaded..');
 end
 %% 
 for i = FIGURENUMBERS
     figure(i); clf; hold on;
 end
 
-%%
+%% Gather data
+% Loop over datasets, then over individual schnitzes. Create a parameter
+% structure that looks as follows:
+%
+% - myLengthNewborns{datasetIdx}(schnitz) 
+% - myLengthParents{datasetIdx}(schnitz)
+% - myLengthSumNewborns{datasetIdx}(schnitz)
+%
+% Where myLengthNewborns gives the length of a newborn, and myLengthParents
+% gives the length of the corresponding parent. Because there are some
+% subtleties (growth, geometrical distortion poles), myLengthSumNewborns is
+% a better measure of the total length before division, and is simply the
+% summed length of the two daughter cells.
+
+lifeTimes = {}; 
+allLengths = {};
 
 for datasetIdx = 1:numel(datasetsPaths)
     
@@ -91,14 +112,22 @@ for datasetIdx = 1:numel(datasetsPaths)
             myLengthNewborns{datasetIdx}(end+1) =     lengthDaughterSchnitz2;
             myLengthParents{datasetIdx}(end+1) =      LengthParent;
             myLengthSumNewborns{datasetIdx}(end+1) =  lengthDaughterSchnitz1+lengthDaughterSchnitz2;
-
+            
         end
 
     end
 
+    %% Gather data on interdivision time vs. lifetime of cell
+    lifeTimes{datasetIdx} = [schnitzcells.interDivTime];
+    allLengths{datasetIdx} = NaN(1,numel(schnitzcells));
+    for i = 1:numel(schnitzcells)
+        thisSchnitzLengths = schnitzcells(i).(LENGTHFIELD);
+        allLengths{datasetIdx}(i) = thisSchnitzLengths(1);
+    end
+    
 end
 
-%% Now plot this
+%% Now calculate ratios and plot them
 SANITYCHECKSYMMETRY=1;
 
 Ratios={};
@@ -150,7 +179,7 @@ for datasetIdx = 1:numel(datasetsPaths)
     ylim([0,1]);
     xlim([LEFTX,RIGHTX]);
 
-    xlabel(['Cell size by ' LENGTHFIELD],'Interpreter','None');
+    xlabel(['Parent cell size by ' LENGTHFIELD],'Interpreter','None');
     ylabel('L_{child}/L_{parent}');
 
     MW_makeplotlookbetter(15)
@@ -220,7 +249,141 @@ axis equal;
 xlim([0 max([myLengthSumNewborns{:}])]);
 ylim([0 max([myLengthParents{:}])]);
 
+%% Plot historgrams per window
+NRREGIMES = 4;
+WINDOWBORDERS = [5,10,17,20,30];
+WINDOWBORDERS = [2,9,16,23,30];
+WINDOWBORDERS = [2:7:30];
+LINEWIDTH =2;
 
+% figure stuff
+h=figure(FIGURENUMBERS(end)+3); clf; hold on
+myColors = linspecer(numel(WINDOWBORDERS)-1);
+
+% calculate params
+mybins = linspace(0,1,HISTNRBINS);
+
+for datasetIdx = 1:numel(datasetsPaths)
+    %datasetIdx=1; % TEMP REMOVE   
+
+    for windowIndex = 1:(numel(WINDOWBORDERS)-1)
+
+        windowLeft = WINDOWBORDERS(windowIndex)
+        windowRight  = WINDOWBORDERS(windowIndex+1)
+        windowSize = windowRight-windowLeft;
+
+        % get data
+        currentWindowIndices = ...
+            find((myLengthSumNewborns{datasetIdx}>windowLeft) == (myLengthSumNewborns{datasetIdx}<windowRight));
+        currentTotalLength = myLengthSumNewborns{datasetIdx}(currentWindowIndices);
+        currentRatios = Ratios{datasetIdx}(currentWindowIndices);
+
+        % top row
+        % ===
+        subplot(2,1,1); hold on
+        plot(currentTotalLength,currentRatios,'o','Color',myColors(windowIndex,:),'MarkerFaceColor',myColors(windowIndex,:))
+        % cosmetics
+        ylim([0,1]);
+        xlim([LEFTX,RIGHTX]);
+        xlabel(['Parent cell size by ' LENGTHFIELD],'Interpreter','None');
+        ylabel('L_{child}/L_{parent}');
+        MW_makeplotlookbetter(15)
+        title([WHATDATA ' condition']);
+        
+        % bottom row with hists
+        % ===
+        subplot(2,1,2); hold on;    
+        % predicted location        
+        nrLocations = windowIndex;
+        for j = 1:2:(nrLocations*2-1)
+            plot([windowLeft, windowRight], [(j)/(2*nrLocations) (j)/(2*nrLocations)],'-','Color',[.5 .5 .5],'LineWidth',2);
+        end        
+        % histograms
+        [counts,centers]=hist(currentRatios,mybins);
+        modcounts = (windowSize*counts/sum(counts)+windowLeft);
+        plot(modcounts,centers,'-','Color',myColors(windowIndex,:),'LineWidth',LINEWIDTH)
+        % cosmetics
+        ylim([0,1]);
+        xlim([LEFTX,RIGHTX]);
+        xlabel(['Histograms (normalized)']);
+        ylabel('L_{child}/L_{parent}');
+        MW_makeplotlookbetter(15)
+        set(gca,'xtick',[])
+
+    end
+end
+
+saveas(h, [PLOTSAVEDIR WHATDATA '_ratios_histograms.fig']);
+saveas(h, [PLOTSAVEDIR WHATDATA '_ratios_histograms.tif']);
+
+%% Plot lifetime against birth length
+
+h=figure(FIGURENUMBERS(end)+4); clf; hold on
+myColors = linspecer(numel(datasetsPaths));
+myPlotMarkers = 'os^y';
+
+title([WHATDATA ' condition']);
+
+%
+meanInterDivisionTimes=[];
+for datasetIdx = 1:numel(datasetsPaths)
+
+    plot(allLengths{datasetIdx},lifeTimes{datasetIdx},'.',...
+        'Marker',myPlotMarkers(datasetIdx),...
+        'LineWidth',2,...
+        'Color',myColors(datasetIdx,:));%,'MarkerFaceColor',myColors(datasetIdx,:));
+    
+    currentInterDivTimes = lifeTimes{datasetIdx};
+    meanInterDivisionTimes(datasetIdx)=mean(currentInterDivTimes(~isnan(currentInterDivTimes)));            
+        % note meanInterDivisionTimes is subject to how long cells where in
+        % filamented state in this dataset.
+    
+end
+%title(num2str(meanInterDivisionTimes))
+
+% Plot the means per window (which was set in earlier plot)
+for datasetIdx = 1:numel(datasetsPaths)
+    
+    %allSetsLifeTimes = [lifeTimes{:}];
+    %allSetsallLengths = [allLengths{:}];
+    
+    meansLengths=[]; meansLifeTimes=[]; stdLifeTimes = [];
+    for windowIndex = 1:(numel(WINDOWBORDERS)-1)
+
+        windowLeft = WINDOWBORDERS(windowIndex);
+        windowRight  = WINDOWBORDERS(windowIndex+1);
+        windowSize = windowRight-windowLeft;
+
+        % get data
+        currentWindowIndices = ...
+            find((allLengths{datasetIdx}>windowLeft) == (allLengths{datasetIdx}<windowRight));
+        currentLengths = allLengths{datasetIdx}(currentWindowIndices);
+        currentLifeTimes = lifeTimes{datasetIdx}(currentWindowIndices);
+
+        % determine nan values
+        indicesBothNoNaN = ~isnan(currentLengths) & ~isnan(currentLifeTimes);
+
+        % determine means
+        meansLengths(end+1) = mean(currentLengths(indicesBothNoNaN));
+        meansLifeTimes(end+1) = mean(currentLifeTimes(indicesBothNoNaN));   
+        stdLifeTimes(end+1) =  std(currentLifeTimes(indicesBothNoNaN));   
+
+    end
+
+    % plot
+    %errorbar(meansLengths,meansLifeTimes,stdLifeTimes,'ok','MarkerFaceColor','k')
+    plot(meansLengths,meansLifeTimes,'-k',...
+        'Marker',myPlotMarkers(datasetIdx),...
+        'MarkerFaceColor','k','MarkerSize',10)
+end
+    
+xlim([LEFTX,RIGHTX]);
+xlabel(['Birth size by ' LENGTHFIELD ' '],'Interpreter','None');
+ylabel('Lifetime [min]');
+MW_makeplotlookbetter(15)
+
+saveas(h, [PLOTSAVEDIR WHATDATA '_lifetime_histograms.fig']);
+saveas(h, [PLOTSAVEDIR WHATDATA '_lifetime_histograms.tif']);
 
 %%
 %{
