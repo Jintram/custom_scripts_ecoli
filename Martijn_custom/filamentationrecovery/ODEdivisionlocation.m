c2f6369bae05ebf0a0d8ecd3dba0eed2b5de49b3
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2016-06-08 22:09:40 +0200

    (modelFilamentationRecovery)

--- Martijn_custom/filamentationrecovery/ODEdivisionlocation.m
+++ Martijn_custom/filamentationrecovery/ODEdivisionlocation.m
@@ -13,28 +13,10 @@ function [simulatedschnitzcells] = ODEdivisionlocation(simulatedschnitzcells, ti
 
 %% parameters
 
-if ~isfield(parameters,'mu')
-    parameters.mu = 1;    
-end
-if ~isfield(parameters,'divisionTimePerMinute')
-    parameters.divisionTime = 1;    
-end
-if ~isfield(parameters,'divisionSize')
-    parameters.divisionSize = 6;    
-end
-if ~isfield(parameters,'divisionType')
-    parameters.divisionType = 'sizer';    
-end
-if ~isfield(parameters,'fluctuationIntensity')
-    parameters.fluctuationIntensity = 0;    
-end
-if ~isfield(parameters,'relaxationTimeFluctuationsMinutes')
-    parameters.relaxationTimeFluctuationsMinutes = 1;    
-end
+% overflow prevention errors
+MAXNUCLEOIDSINCELL = 1000;
+MAXSCHNITZESTOMAKE = 10000;
 
-parameters.lambda=parameters.mu*log(2);
-parameters.lambdaperminute = parameters.lambda/60;
-parameters.divisionTimePerMinute = parameters.divisionTime*60;
     
     
 %% Go over active schnitzcells
@@ -44,7 +26,7 @@ activeSchnitzcells = find( ([simulatedschnitzcells(:).D] == 0) & ([simulatedschn
 
 for schnitzIdx = activeSchnitzcells
       
-    %% ODEs
+    %% ODEs ---------------------------------------------------------------
     deltaGrowthEfficiency = -(simulatedschnitzcells(schnitzIdx).growthEfficiencies(end)-1)*1/parameters.relaxationTimeFluctuationsMinutes + ... % damping; 1 is the equilibrium value
                             ((rand()-.5)*2)*parameters.fluctuationIntensity; % noise component
 
@@ -59,76 +41,230 @@ for schnitzIdx = activeSchnitzcells
     newCellLength        = ...
         simulatedschnitzcells(schnitzIdx).cellLengths(end) + deltacellLength;
     
-    % determine potential division sites
+    % generally, nr. nucleoids and rings are identical to last frame
+    newNrNucleoids = ...
+        simulatedschnitzcells(schnitzIdx).nrNucleoids(end);
     newRingSites = ...
         simulatedschnitzcells(schnitzIdx).relativeRingSites{end};
     
-    %% cell division
+    % determine last nucleoid duplication event
+    nucleoidDuplicationEvents=[]; checkoutSchnitz = schnitzIdx;
+    while isempty(nucleoidDuplicationEvents)
+        nucleoidDuplicationEvents = simulatedschnitzcells(checkoutSchnitz).nucleoidDuplicationEvents;
+        checkoutSchnitz = simulatedschnitzcells(checkoutSchnitz).P;
+    end
+    lastDuplicationEvent=nucleoidDuplicationEvents(end);
+    
+    % determine last division event
+    lastDivisionEvent = simulatedschnitzcells(schnitzIdx).times(1);
+    
+    
+    %% determine whether nucleoid duplication & cell division should happen
     % dt = 1 minute, and is implicit
     
+    % Do not duplicate nucleoids, unless..
+    nucleoidDuplication=0;
     % Do not divide, unless..
     divide=0;
-       
-    % sizer
-    if strcmp(parameters.divisionType,'sizer')
-        if newCellLength>parameters.divisionSize
+           
+    % Determine according to divison type of the cell
+    if strcmp(parameters.divisionType,'nucleoidsizer') 
+    
+        % nucleoid sizer
+        % ===
+    
+        % nucleoid duplication
+        if newCellLength>(parameters.divisionSize)  && ... % sizer condition
+            (newtime-(lastDuplicationEvent)) > (parameters.replicationTimeInMinutes) % timer condition
+        
+            nucleoidDuplication=1;
+            lastDuplicationEvent=newtime;
+        end
+        if parameters.divisionDelayInMinutes< (newtime-lastDuplicationEvent) ... % delay after duplication
+                && parameters.rechargeTimeInMinutes< (newtime-lastDivisionEvent) ... delay after previous division
+                && ~parameters.divisionBlock
+        % cell division
+        %if newCellLength>parameters.divisionSize
             divide = 1;   
+        end    
+    
+    elseif strcmp(parameters.divisionType,'nucleoidadder')
+    
+        % nucleoid adder
+        % ===
+        
+        lastDivisionSize = simulatedschnitzcells(schnitzIdx).cellLengths(1);
+        
+        % nucleoid duplication
+        %if newCellLength>parameters.divisionSize
+        if (newCellLength-lastDivisionSize)>(parameters.addedSize) && ... % adder condition
+            (newtime-(lastDuplicationEvent)) > (parameters.replicationTimeInMinutes) % timer condition
+        
+            nucleoidDuplication=1;
+            lastDuplicationEvent=newtime;
         end
-    end
-
-    % adder
-    if strcmp(parameters.divisionType,'adder')
-        if newCellLength>parameters.divisionSize
-            lastDivisionSize = simulatedschnitzcells(schnitzIdx).cellLengths(1);
-            if (newCellLength-lastDivisionSize)>parameters.addedSize
-                divide=1;
-            end
-        end
-    end
-
-    % timer
-    if strcmp(parameters.divisionType,'timer')
-        if (time-newtime)>parameters.divisionTimePerMinute
+        %end
+        
+        % cell division
+        %if newCellLength>parameters.divisionSize
+        if parameters.divisionDelayInMinutes< (newtime-lastDuplicationEvent)... % delay after duplication
+                && parameters.rechargeTimeInMinutes< (newtime-lastDivisionEvent) ... delay after previous division
+                && ~parameters.divisionBlock
+        %if (newCellLength-lastDivisionSize)>parameters.addedSize
             divide=1;
         end
-    end
+        %end
+        
+    elseif strcmp(parameters.divisionType,'nucleoidtimer')
+    
+        % nucleoid timer
+        % ===
+    
+        % nucleoid duplication
+        if (newtime-(lastDuplicationEvent)) > (parameters.replicationTimeInMinutes) % timer condition
+            
+            nucleoidDuplication=1;
+            lastDuplicationEvent=newtime;
+        end
+        % division
+        %if (simulatedschnitzcells(schnitzIdx).times(1)-newtime)>parameters.divisionTimePerMinute
+        if parameters.divisionDelayInMinutes< (newtime-lastDuplicationEvent) ... % delay after duplication
+                && parameters.rechargeTimeInMinutes< (newtime-lastDivisionEvent) ... delay after previous division
+                && ~parameters.divisionBlock
+            divide=1;
+        end
+        
+    elseif strcmp(parameters.divisionType,'simplesizer')        
+        
+        if newCellLength>(parameters.divisionSize) ... % simple sizer condition
+                && parameters.rechargeTimeInMinutes< (newtime-lastDivisionEvent) ... % delay after previous division
+                && ~parameters.divisionBlock % cell division inhibition
+            divide=1;
+        end
+        
+    end    
+    
+    
 
-    % execute division if determined so
+    %% execute duplication event if determined so
+        % nucleoid replication that results in more rings (assuming all
+    % nucleoids divide in phase and are thus automatically aligned with 
+    % rings).        
+    if nucleoidDuplication
+               
+        % duplicate nucleoid number
+        newNrNucleoids = newNrNucleoids*2;        
+        
+        % update duplication event list
+        simulatedschnitzcells(schnitzIdx).nucleoidDuplicationEvents(end+1) = newtime;        
+    end
+    
+    %% Update rings accordingly -------------------------------------------
+    
+    if strcmp(parameters.ringType,'nucleoid')
+        
+        % rings follow nucleoid number
+        if nucleoidDuplication            
+            nrRings = newNrNucleoids-1;        
+            newRingSites = (1:nrRings)/(nrRings+1);
+        end
+    
+    elseif strcmp(parameters.ringType,'alwaysmiddle')
+        
+        if divide
+            newRingSites = .5;
+        end
+        
+    elseif strcmp(parameters.ringType,'alwaystypical')
+        
+        % rings are such that resulting length closest to (multiple) typical
+        nrRings = round(newCellLength/(parameters.divisionSize/2))-1;
+        newRingSites = (1:nrRings)/(nrRings+1);
+    
+    elseif strcmp(parameters.ringType,'dynamicalwaystypical')
+        
+        % rings are such that resulting length closest to (multiple) typical
+        nrRings = round(newCellLength/(parameters.divisionSize/2))-1;
+        newRingSites = (1:nrRings)/(nrRings+1);
+        newRingSites = newRingSites(find(round(rand(numel(newRingSites),1)))); % select half of them to simulate dynamic behavior
+        
+    elseif strcmp(parameters.ringType,'alwaysrandom')
+        
+        marginAtSides = (parameters.divisionSize/4 * .99) / newCellLength;
+        widthOfPotentialRingSites = 1-2*marginAtSides;
+        newRingSites = rand()*widthOfPotentialRingSites+marginAtSides;
+        
+    end
+    
+    %% execute division if determined so ----------------------------------
+    % note that life for a cell starts here, so the time is recorded in the
+    % new cells, but not in the old ones.
+    divided=0;
     if divide
         
-        % create daughter indices
-        newIdx1 = numel(simulatedschnitzcells)+1;
-        newIdx2 = numel(simulatedschnitzcells)+2;
+        % choose (at random) at which ring to divide
+        nrRings = numel(newRingSites);
         
-        % link mother and daughters
-        simulatedschnitzcells(schnitzIdx).D=newIdx1;
-        simulatedschnitzcells(schnitzIdx).E=newIdx2;
-        simulatedschnitzcells(newIdx1).P=schnitzIdx;
-        simulatedschnitzcells(newIdx2).P=schnitzIdx;
+        % divide if there's a ring
+        if nrRings > 0
+            selectedRing = ceil(rand*nrRings);
+            ratioWhereToDivide = newRingSites(selectedRing);
+
+            % create daughter indices
+            newIdx1 = numel(simulatedschnitzcells)+1;
+            newIdx2 = numel(simulatedschnitzcells)+2;
+
+            % link mother and daughters
+            simulatedschnitzcells(schnitzIdx).D=newIdx1;
+            simulatedschnitzcells(schnitzIdx).E=newIdx2;
+            simulatedschnitzcells(newIdx1).P=schnitzIdx;
+            simulatedschnitzcells(newIdx2).P=schnitzIdx;
+
+            % New cells don't have children
+            simulatedschnitzcells(newIdx1).D=0;
+            simulatedschnitzcells(newIdx1).E=0;
+            simulatedschnitzcells(newIdx2).D=0;
+            simulatedschnitzcells(newIdx2).E=0;
+
+            % calculate daughter cell lengths
+            lengthBeforeDivision = newCellLength;
+            lengthAfterDivision1 = lengthBeforeDivision*ratioWhereToDivide;
+            lengthAfterDivision2 = lengthBeforeDivision-lengthAfterDivision1; %lengthBeforeDivision*(1-ratioWhereToDivide);
+            simulatedschnitzcells(newIdx1).cellLengths=lengthAfterDivision1;
+            simulatedschnitzcells(newIdx2).cellLengths=lengthAfterDivision2;
+            % daughter growthEfficiencies
+            simulatedschnitzcells(newIdx1).growthEfficiencies=[newGrowthEfficiency];
+            simulatedschnitzcells(newIdx2).growthEfficiencies=[newGrowthEfficiency];
+            % daughter times
+            simulatedschnitzcells(newIdx1).times=time;
+            simulatedschnitzcells(newIdx2).times=time;     
+            % daughter ring reorganization times
+            simulatedschnitzcells(newIdx1).ringReorganizationTimes = [];
+            simulatedschnitzcells(newIdx1).ringReorganizationTimes = [];
+            % daughter nr of nucleoids (assume distributed by length)
+            nrNucleoidsForCell1 = round(newNrNucleoids*lengthAfterDivision1/lengthBeforeDivision);
+            simulatedschnitzcells(newIdx1).nrNucleoids = nrNucleoidsForCell1;
+            simulatedschnitzcells(newIdx2).nrNucleoids = newNrNucleoids-nrNucleoidsForCell1;
+            % nucleoid division boolean event list
+            simulatedschnitzcells(newIdx1).nucleoidDuplicationEventsBoolean(end+1) = nucleoidDuplication;
+            simulatedschnitzcells(newIdx2).nucleoidDuplicationEventsBoolean(end+1) = nucleoidDuplication;
+            
+            
+            % daughter ring sites        
+            % (compensate for )
+            simulatedschnitzcells(newIdx1).relativeRingSites = ...
+                { newRingSites(1:selectedRing-1)   .* lengthBeforeDivision / lengthAfterDivision1};
+            simulatedschnitzcells(newIdx2).relativeRingSites = ...
+                { ((newRingSites(selectedRing+1:end) .* lengthBeforeDivision) - lengthAfterDivision1) / lengthAfterDivision2};
+
+            divided=1;
+        else
+            disp('No ring present to divide..');
+        end
         
-        % New cells don't have children
-        simulatedschnitzcells(newIdx1).D=0;
-        simulatedschnitzcells(newIdx1).E=0;
-        simulatedschnitzcells(newIdx2).D=0;
-        simulatedschnitzcells(newIdx2).E=0;
+    end
         
-        % calculate daughter cell lengths
-        simulatedschnitzcells(newIdx1).cellLengths=[newCellLength*newRingSites];
-        simulatedschnitzcells(newIdx2).cellLengths=[newCellLength*(1-newRingSites)];
-        % daughter growthEfficiencies
-        simulatedschnitzcells(newIdx1).growthEfficiencies=[newGrowthEfficiency];
-        simulatedschnitzcells(newIdx2).growthEfficiencies=[newGrowthEfficiency];
-        % daughter times
-        simulatedschnitzcells(newIdx1).times=time;
-        simulatedschnitzcells(newIdx2).times=time;            
-    
-        % daughter ring sites        
-        simulatedschnitzcells(newIdx1).relativeRingSites = ...
-            {newRingSites};
-        simulatedschnitzcells(newIdx2).relativeRingSites = ...
-            {newRingSites};
-        
-    else       
+    if ~divided 
         
         % propagate normally
         simulatedschnitzcells(schnitzIdx).times(end+1) = newtime;
@@ -140,8 +276,22 @@ for schnitzIdx = activeSchnitzcells
         simulatedschnitzcells(schnitzIdx).relativeRingSites{end+1} = ...
             newRingSites;
         
+        simulatedschnitzcells(schnitzIdx).nrNucleoids(end+1) = ...
+            newNrNucleoids;
+
+        simulatedschnitzcells(schnitzIdx).nucleoidDuplicationEventsBoolean(end+1) = ...
+            nucleoidDuplication;
+        
     end
     
+    if newNrNucleoids > MAXNUCLEOIDSINCELL
+        error('Overflow protection error (nucleoids).');
+    end
+    
+end
+
+if numel(simulatedschnitzcells)>MAXSCHNITZESTOMAKE
+    error('Overflow protection error (schnitznr).');
 end
 
 
--- Martijn_custom/filamentationrecovery/propagateODEdivisionlocation.m
+++ Martijn_custom/filamentationrecovery/propagateODEdivisionlocation.m
@@ -1,30 +1,79 @@
 
 %%
-parameters.mu = 1;
-parameters.divisionTime = 1; % for timer model to work, this needs to be equal to mu
-parameters.divisionSize = 6;
-parameters.addedSize = 6;
-parameters.divisionType = 'sizer';
+
+PLOTOUTPUTDIRROOT = 'D:\Local_Software\Martijn_extensions\Martijn_custom\filamentationrecovery\divisionmodel_plots\';
+if ~exist('SAVEYESNO','var')
+    SAVEYESNO=0; 
+    % SAVEYESNO=1;
+end
+
+if ~exist('parameters','var')
+    parameters.mu = 1.5; % dbls/hr
+    parameters.replicationTime = 1/1.5; % hr/dbl
+    parameters.replicationPhase = .1;
+    parameters.divisionTime = 1; % for timer model to work, this needs to be equal to 1/mu
+    parameters.divisionSize = 6;
+    parameters.addedSize = 3;
+    parameters.divisionType = 'nucleoidsizer'; % simplesizer nucleoidsizer nucleoidadder
+    parameters.ringType = 'nucleoid'; % nucleoid alwaysrandom alwaysmiddle alwaystypical dynamicalwaystypical
+    parameters.divisionDelay = parameters.mu/10; % 0
+    parameters.rechargeTime = parameters.mu/200; % 20
+    parameters.divisionBlock=0;
+    % clear parameters
+end
 
 % noise parameters
-parameters.fluctuationIntensity = 0.04; % 0.04 seems good value
+parameters.fluctuationIntensity = 0.0; % 0.04 seems good value
 parameters.relaxationTimeFluctuationsMinutes = 50; % 50 seems good value
 
-INITIALLENGTH=25;
+INITIALLENGTH=3;
+FIG3XLIM=[100 200];
+SWITCHTIME = 150; % 150
 
-simulationtimes = 1:100; % simulation time
+SHOWNUCLEOIDS = 0;
 
-% Create start of lineage
+simulationtimes = 1:200; % simulation time
+
+% extrapolated parametes
+parameters.replicationTimeInMinutes = parameters.replicationTime*60;
+parameters.lambda = parameters.mu*log(2); % /hr
+parameters.lambdaperminute = parameters.lambda/60;
+parameters.divisionTimePerMinute = parameters.divisionTime*60;
+parameters.divisionDelayInMinutes = parameters.divisionDelay*60;
+parameters.rechargeTimeInMinutes = parameters.rechargeTime*60;
+
+% Initial conditions: create start of lineage
 simulatedschnitzcells = {};
 simulatedschnitzcells(1).P = -1; % parent
 simulatedschnitzcells(1).D = 0; simulatedschnitzcells(1).E = 0; % daughters
 simulatedschnitzcells(1).cellLengths = [INITIALLENGTH];
 simulatedschnitzcells(1).growthEfficiencies = [1];
-simulatedschnitzcells(1).relativeRingSites = {[.5]};
+simulatedschnitzcells(1).relativeRingSites = {[]};
 simulatedschnitzcells(1).times = [simulationtimes(1)];
+simulatedschnitzcells(1).nucleoidDuplicationEvents = [simulationtimes(1)- (1-parameters.replicationPhase).*parameters.replicationTimeInMinutes];
+simulatedschnitzcells(1).nrNucleoids = [1];
+simulatedschnitzcells(1).nucleoidDuplicationEventsBoolean=[1];
+
+%% Establish output directory if desired
+
+if SAVEYESNO==1
+
+    currentDateTime = datestr(now);
+    currentDateTime = strrep(currentDateTime, ':', '-');
+    currentDateTime = strrep(currentDateTime, ' ', '_');
+    plotoutputdir = [PLOTOUTPUTDIRROOT currentDateTime '\'];
+
+    if ~exist(plotoutputdir)
+        mkdir(plotoutputdir)
+    end
+end
+
 
 %%
-for time = simulationtimes(2:end)
+
+% filamentation part of simulation
+parameters.divisionBlock=1;
+for time = simulationtimes(2:SWITCHTIME-1)
     [simulatedschnitzcells] = ODEdivisionlocation(simulatedschnitzcells, time, parameters);
     
     if mod(time,50)==0
@@ -32,9 +81,20 @@ for time = simulationtimes(2:end)
     end
 end
 
+% recovery part of simulation
+parameters.divisionBlock=0;
+for time = simulationtimes(SWITCHTIME:end)
+    [simulatedschnitzcells] = ODEdivisionlocation(simulatedschnitzcells, time, parameters);
+    
+    if mod(time,50)==0
+        disp(['Simulated up to ' num2str(time) ' of ' num2str(simulationtimes(end)) '..']);
+    end
+end
+
+
 %%
 
-h=figure(1); clf; hold on;
+h1=figure(1); clf; hold on;
 plot([simulatedschnitzcells(:).times],[simulatedschnitzcells(:).cellLengths],'.');
 
 plot([min(simulationtimes),max(simulationtimes)],[parameters.divisionSize,parameters.divisionSize],'-','Color',[.5 .5 .5])
@@ -42,19 +102,26 @@ plot([min(simulationtimes),max(simulationtimes)],[parameters.divisionSize,parame
 
 
 %% kymograph
-h=figure(2); clf; hold on;
+h2=figure(2); clf; hold on;
 
 % calculate ring location in absolute length values
 for loopIdx = 1:numel(simulatedschnitzcells)   
     
     for idx = 1:numel(simulatedschnitzcells(loopIdx).times)
+        
+        % absolute ring sites
         simulatedschnitzcells(loopIdx).absoluteRingSite{idx}=...
             simulatedschnitzcells(loopIdx).cellLengths(idx).*simulatedschnitzcells(loopIdx).relativeRingSites{idx};
+        
+        % absolute nucleoid sites
+        simulatedschnitzcells(loopIdx).absoluteNucleoidSite{idx}=...
+            simulatedschnitzcells(loopIdx).cellLengths(idx).*(1:simulatedschnitzcells(loopIdx).nrNucleoids(idx))/(simulatedschnitzcells(loopIdx).nrNucleoids(idx)+1);
+                
     end
     
 end
 
-for time = simulationtimes(1:100)
+for time = simulationtimes
     
     indicesCurrentTime = find([simulatedschnitzcells(:).times] == time);
     
@@ -128,6 +195,7 @@ for i = 1:numel(simulatedschnitzcells)
     end
 end
 
+tic;
 % now expand lookuptable
 for t = 2:numel(simulationtimes)
     
@@ -161,12 +229,22 @@ for t = 2:numel(simulationtimes)
             lookuptable{t}(rprime,2) = find((simulatedschnitzcells(nextSchnitz).times==simulationtimes(t)));
         end
             
+        if toc>10
+            error('timeout error');
+        end
+        
     end
     
 end
 
 %%
-h=figure(3); clf; hold on;
+h3=figure(3); clf; hold on;
+% dummy markers for legend (appear at t<0)
+l1=plot(-1,0,'sk-','MarkerFaceColor','k','MarkerSize',5);
+l2=plot(-1,0,'or','MarkerSize',4,'MarkerFaceColor','r');
+l3=plot(-1,0,'ob','MarkerSize',4);
+
+tic
 for t=1:numel(lookuptable)
     
     currentSchnitzes = lookuptable{t}(:,1)';
@@ -179,7 +257,7 @@ for t=1:numel(lookuptable)
     
     previousLengthsSummed=0;
     plottingDividedLocations = [-totalLength/2];
-    plottingRingLocations = [];
+    plottingRingLocations = []; plottingNucleoidLocations= [];
     for loopIdx = 1:numel(currentSchnitzes)
     
         plottingDividedLocations = ...
@@ -189,12 +267,21 @@ for t=1:numel(lookuptable)
             plottingRingLocations = ...
                 [plottingRingLocations previousLengthsSummed+simulatedschnitzcells(currentSchnitzes(loopIdx)).absoluteRingSite{currentFrames(loopIdx)}(ringIdx)-totalLength/2];
         end
+
+        for nucleoidIdx = 1:numel(simulatedschnitzcells(currentSchnitzes(loopIdx)).absoluteNucleoidSite{currentFrames(loopIdx)})
+            plottingNucleoidLocations = ...
+                [plottingNucleoidLocations previousLengthsSummed+simulatedschnitzcells(currentSchnitzes(loopIdx)).absoluteNucleoidSite{currentFrames(loopIdx)}(nucleoidIdx)-totalLength/2];
+        end
         
         %plot(simulatedschnitzcells(currentSchnitzes(loopIdx)).times(currentFrames(loopIdx)),...
         %     previousLengthsSummed+simulatedschnitzcells(currentSchnitzes(loopIdx)).cellLengths(currentFrames(loopIdx))-totalLength/2,...
         %    'ok-','MarkerFaceColor','k','MarkerSize',5);
         
         previousLengthsSummed=previousLengthsSummed+simulatedschnitzcells(currentSchnitzes(loopIdx)).cellLengths(currentFrames(loopIdx));
+       
+        if toc>10
+            error('timeout error');
+        end
         
     end
     
@@ -203,26 +290,80 @@ for t=1:numel(lookuptable)
            'sk-','MarkerFaceColor','k','MarkerSize',5);
     plot(ones(1,numel(plottingRingLocations))*simulationtimes(t),...
             plottingRingLocations,...
-           'or','MarkerSize',4);
+           'or','MarkerSize',4,'MarkerFaceColor','r');
+    if SHOWNUCLEOIDS
+        plot(ones(1,numel(plottingNucleoidLocations))*simulationtimes(t),...
+                plottingNucleoidLocations,...
+               'ob','MarkerSize',4);
+    end
+end
+
+currentYlim=ylim;
+plot([SWITCHTIME,SWITCHTIME], currentYlim,'--k');
+
+xlim(FIG3XLIM)
+
+xlabel('Time');
+ylabel('Cells [um]');
+
+if SHOWNUCLEOIDS
+    legend([l1,l2,l3],{'Cells','Division ring','Nucleoid'},'Location','SouthWest'); % SouthWest Best
+else
+    legend([l1,l2],{'Cells','Division ring'},'Location','SouthWest'); % SouthWest Best
+end
+warning('This does not always work because lines are not always plotted during above loop..');
+
+MW_makeplotlookbetter(20);
+%%
+if SAVEYESNO
+    saveas(h3,[plotoutputdir 'cellview.fig']);
+    saveas(h3,[plotoutputdir 'cellview.tif']);
+end
+
+%% histogram of sizes
+
+h4=figure(4); clf; hold on;
+
+allLengths = [simulatedschnitzcells(:).cellLengths];
+[counts, locations]=hist(allLengths,30);
+meanLength = mean(allLengths);
+medianLength = median(allLengths);
+plot(locations, counts);
+plot([medianLength,medianLength],[0,max(counts)],'-r')
+title(['median = ' num2str(medianLength)]);
+
+if SAVEYESNO
+    saveas(h4,[plotoutputdir 'cellsizes.fig']);
+    saveas(h4,[plotoutputdir 'cellsizes.tif']);
+end
+
+%% Save parameter struct
+if SAVEYESNO
+    save([plotoutputdir 'parameters.mat'],'parameters');
+end
+
+% output parameters to text file
+stringwithdata = '';
+allFieldNames = {fieldnames(parameters)};
+for i = 1:numel(allFieldNames{1})
+    fieldName = allFieldNames{1}{i}
+    fieldValue = parameters.(fieldName);
+    stringwithdata = [stringwithdata 10 fieldName '=' num2str(fieldValue)];
+end
+stringwithdata
+%%
+if SAVEYESNO
+    fid = fopen([plotoutputdir 'parameters.txt'],'w');
+    fprintf(fid, stringwithdata);
+    fclose(fid);
 end
 
 
-%{
-figure(1); clf; hold on;
-plot(times,cellLengths,'LineWidth',3)
 
-plot(divisionEvents,zeros(1,numel(divisionEvents)),'^r','MarkerFaceColor','r','MarkerSize',15)
 
-xlabel('time (min)');
-ylabel('cellength [\mum]');
-MW_makeplotlookbetter(20);
 
-figure(2); clf; hold on;
-plot(times,growthEfficiencies,'LineWidth',3)
 
-plot(divisionEvents,zeros(1,numel(divisionEvents)),'^r','MarkerFaceColor','r','MarkerSize',15)
 
-xlabel('time (min)');
-ylabel('growth efficiency');
-MW_makeplotlookbetter(20);
-%}
+
+
+
