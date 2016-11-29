d989a05846a698fe80890c843ddf478f3ad4cd6e
Antoni van Leeuwenhoek <wehrens@AMOLF.nl>
2016-11-29 11:50:01 +0100

    (misc)

--- Martijn_custom/singlecell_scriptsperexperiment/script20160920_CFPdivYFP_riboswitchrrsa.m
+++ Martijn_custom/singlecell_scriptsperexperiment/script20160920_CFPdivYFP_riboswitchrrsa.m
@@ -13,14 +13,30 @@ MW_summaryplotspreliminaryanalysis
 MW_summaryplotspreliminaryanalysisforswitch
 %}
 
-if ~ishandle(hratio); hratio=figure(); else figure(hratio); end
+if ~exist('hratio','var'), hratio=figure(); elseif ~ishandle(hratio); hratio=figure(); figure(hratio); end 
 clf; hold on;
 
+datapile=[];
 for i=1:3
-    plot([alldata(i).frameData(:).frameTime],fluorValuesAll{i}(1,:)./fluorValuesAll{i}(2,:),'LineWidth',3)
+    xdata=[alldata(i).frameData(:).frameTime];
+    ydata=[fluorValuesAll{i}(1,:)./fluorValuesAll{i}(2,:)];
+    plot(xdata(~isnan(ydata)),...
+         ydata(~isnan(ydata)) ,'-x','LineWidth',3)
+    datapile=[datapile ydata(~isnan(ydata))];
 end
+
+theYlim=[0, max(datapile)*1.2];
+ylim(theYlim)
+
+%plot switchtime
+plot([SWITCHTIME,SWITCHTIME],theYlim,':k','LineWidth',3);
 
 title('Ratio between fluor values')
 xlabel('time [min]');
 ylabel(['mCerulean / mVenus' 10 'or prrsa / pn25']);
 MW_makeplotlookbetter(20);
+
+outputFilepath = [MYDIR 'summaryplot_ceruleanDivVenus'];
+saveas(hratio, [outputFilepath '.tif']);
+saveas(hratio, [outputFilepath '.fig']);
+
